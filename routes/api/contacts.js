1b492b3ae8747d6a1e2441e380b5940c2b519fd1
LenaCScheper <ven.lenysja@ukr.net>
2022-06-05 16:52:58 +0300

    добавила скрипт

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,113 @@
-const express = require('express')
+const express = require('express');
+const router = express.Router();
+const Joi = require('joi');
 
-const router = express.Router()
+const { Contact } = require('../../models/index');
+
+const joiScheme = Joi.object({
+	name: Joi.string().required(),
+	email: Joi.string().required(),
+	phone: Joi.string().required(),
+});
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+	try {
+		const contacts = await Contact.find();
+		res.json(contacts);
+	} catch (error) {
+		next(error);
+	}
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+	const { contactId } = req.params;
+	try {
+		const contact = await Contact.findById(contactId);
+		if (!contact) {
+			const error = new Error('Not found');
+			error.status = 404;
+			throw error;
+		}
+		res.json(contact);
+	} catch (error) {
+		next(error);
+	}
+});
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+	try {
+		const { error } = joiScheme.validate(req.body);
+		if (error) {
+			error.status = 400;
+			throw error;
+		}
+		const newContact = await Contact.create(req.body);
+		res.status(201).json(newContact);
+	} catch (error) {
+		if (error.message.includes('Cast to OdjectId failed')) {
+			error.status = 404;
+		}
+		next(error);
+	}
+});
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+	try {
+		const { contactId } = req.params;
+		const deleteContact = await Contact.findOneAndRemove(contactId);
+		if (!deleteContact) {
+			const error = new Error('Not found');
+			error.status = 404;
+			throw error;
+		}
+		res.json({ message: 'contact deleted' });
+	} catch (error) {
+		next(error);
+	}
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.patch('/:contactId', async (req, res, next) => {
+	try {
+		const { error } = joiScheme.validate(req.body);
+		if (error) {
+			error.status = 400;
+			throw error;
+		}
+		const { contactId } = req.params;
+		const updateContact = await Contact.findByIdAndUpdate({
+			contactId,
+			...req.body,
+		});
+		if (!updateContact) {
+			error.status = 400;
+			throw error;
+		}
+		res.json(updateContact);
+	} catch (error) {
+		next(error);
+	}
+});
 
-module.exports = router
+router.patch('/:id/favorite', async (req, res, next) => {
+	try {
+		const { error } = joiScheme.validate(req.body);
+		if (error) {
+			error.status = 400;
+			throw error;
+		}
+		const { contactId } = req.params;
+		const updateContact = await Contact.findByIdAndUpdate({
+			contactId,
+			...req.body,
+		});
+		if (!updateContact) {
+			error.status = 400;
+			throw error;
+		}
+		res.json(updateContact);
+	} catch (error) {
+		next(error);
+	}
+});
+
+module.exports = router;
